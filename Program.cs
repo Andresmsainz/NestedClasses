910ad396a96afcc5cd11a5eb77ca375291b6ce71
Andres Sainz <andresmsainz@gmail.com>
2021-05-10 22:03:21 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace NestedClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nested Classes!");
+            Animals animal = new Animals();
+
+            //Cannot access Cat or Dog
+            animal.animalCountry = "USA";
+
+            //Declare Dogs within Animals
+            Animals.Dogs dog = new Animals.Dogs();
+            dog.dogBreed = "German Sheperd";
+
+            //Declare Cats within Animals
+            Animals.Cats cat = new Animals.Cats();
+            cat.catName = "Mr Piffles";
+        }
+    }
+
+    class Animals
+    {
+        public string animalName;
+        public string animalCountry;
+
+        public class Dogs
+        {
+            public string dogBreed;
+            public string dogNickname
+        }
+
+        public class Cats
+        {
+            public string catName;
+        }
+        
+    }
+}
